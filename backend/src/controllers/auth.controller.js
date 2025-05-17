0c2e7c17fd6b0e22912aae90926616c4c228593e
Anjany Pandey <anjany.pandey06@gmail.com>
2025-05-18 01:28:19 +0530

    refactor: implement authentication routes and controllers

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req, res) {
+  res.send("sign up route");
+}
+export async function login(req, res) {
+  res.send("login route");
+}
+export async function logout(req, res) {
+  res.send("logout route");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-
-
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Sign up route");
-});
-app.get("/api/auth/login", (req, res) => {
-  res.send("login route");
-});
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout route");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
